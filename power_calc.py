2326c5fb3067d6521f5d277224202edc4bb15799
winterkolin <aomzicy134@gmaill.com>
2025-12-18 15:45:10 +0700

    lastest update

--- power_calc.py
+++ power_calc.py
@@ -2,3 +2,9 @@ v = float(input()) #input the voltage
 r  = float(input()) #input the resistant 
 i = v*r 
 print(f"current is {i} Amplare")
+for i in range(1,10):
+    if i > 5 :
+        v += 1
+    else :
+        v += 2 
+print(f"current is {i} Amplare")
